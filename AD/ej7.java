3d172e620af92e892f513535bd16c2fd849ab14f
dai <usuario@usuario-System-Product-Name.(none)>
2013-09-20 11:09:09 +0200

    corregido ej7 de AD

--- AD/ej7.java
+++ AD/ej7.java
@@ -4,10 +4,13 @@ class ej7 {
 	public static void main(String args[]) {
 		String s = "Esto es una prueba con FileWriter";
 		try {
-			File f = new File("/home/usuario/Documentos/AD/ficherotexto.txt");
+			File f = new File("/home/usuario/Documentos/2dam/AD/ficherotexto.txt");
 			FileWriter fw = new FileWriter(f);
-			for(int i = 0; i < 5; i++) {
-				fw.write(s + "\n");
+			for(int j = 0; j < 3; j++) {
+				for(int i = 0; i < 5; i++) {
+					fw.write(s + "\n");
+				}
+				fw.write("\n");
 			}
 			fw.close();
 		} catch(IOException e) { e.printStackTrace(); }
