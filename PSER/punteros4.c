5294841f1cca77a517a3d7530ea7a1ef90e4481e
Fernando Quilez Esquitino <ferquies@gmail.com>
2013-09-30 12:35:42 +0200

    añadidos ejercicios del 26/09

--- PSER/punteros4.c
+++ PSER/punteros4.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-	int mat[3][5] = {{ 23,1,54,3,14 }, { 5,67,7,8,9 }, { 10,111,12,13,14 }};
 	int i, j;
-	
-	printf("sizeof(int): %d\n", sizeof(int));
+	int mat[3][5];
+
 	for(i = 0; i < 3; i++) {
 		for(j = 0; j < 5; j++) {
-			printf("[%d][%d]. %p\n", i, j, &mat[i][j]);
+			*(*(mat+i)+j) = i+j;
 		}
 	}
 	printf("\n");
