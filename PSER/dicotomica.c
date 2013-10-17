452dce0fd7beeef6a111ebf23ca54ad09892226d
Fernando Quilez Esquitino <ferquies@gmail.com>
2013-10-17 12:53:28 +0200

    añadido dicotomica.c

--- PSER/dicotomica.c
+++ PSER/dicotomica.c
@@ -6,8 +6,8 @@ int main(int argc, char **argv) {
 	int N = 500000;
 	long *array_dinamico = (long*) malloc(N*sizeof(long));
 	double tiempo[100];
-	double media = 0;
-	int i, x, aleatorio;
+	double media_tiempo = 0;
+	int encontrado = 0, i, x, aleatorio, media = N/2;
 	
 	for(i = 0; i < N; i++) {
 		array_dinamico[i] = i;
@@ -21,9 +21,15 @@ int main(int argc, char **argv) {
 		struct tms buf;
 		clock_t t1 = times(&buf);
 	
-		for(i = 0; i < N; i++) {
-			if(array_dinamico[i] == aleatorio) {
+		while(encontrado == 0) {
+			if(aleatorio > array_dinamico[media]) {
+				media /= 2;
+			} else if(aleatorio < array_dinamico[media]) {
+				media = (N - media)/2;
+			} else if(aleatorio == array_dinamico[media]) {
 				break;
+			} else {
+				printf("No encontrado\n");
 			}
 		}
 	
@@ -34,15 +40,14 @@ int main(int argc, char **argv) {
 	}
 	
 	for(x = 0; x < 100; x++) {
-		media += tiempo[x];
+		media_tiempo += tiempo[x];
 	}
 	
-	media = media/100;
+	media_tiempo = media/100;
 	
-	printf("La media de tiempo de todas las busquedas es: %f\n", media); 
+	printf("La media de tiempo de todas las busquedas es: %f\n", media_tiempo); 
 	
 	free(array_dinamico);
 	
 	return 0;
 }
-
