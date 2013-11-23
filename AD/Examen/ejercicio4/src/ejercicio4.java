b3855d1e5207b3c115fafc279d6eb5654c0781da
Fernando Quilez Esquitino <ferquies@gmail.com>
2013-11-23 18:20:42 +0100

    corregida solucion chapuza AD/Examen/ejercicio4/src/ejercicio4.java

--- AD/Examen/ejercicio4/src/ejercicio4.java
+++ AD/Examen/ejercicio4/src/ejercicio4.java
@@ -7,7 +7,6 @@ import com.mysql.jdbc.Connection;
 
 public class ejercicio4 {
 	public static void main(String[] args) {
-		int existe = 0;
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Introduce el ID del autor: ");
 		String id = sc.next();
@@ -19,25 +18,17 @@ public class ejercicio4 {
 		    Class.forName("com.mysql.jdbc.Driver");
 		    Connection conexion = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/autores","root","");
 		    Statement sentencia = conexion.createStatement();
-		    ResultSet result1 = sentencia.executeQuery("SELECT id FROM tb_autores");
-		    while(result1.next()) {
-				if(Integer.parseInt(id) == result1.getInt(1)) {
-					existe = 1;
-				} 
-		    }
-		    result1.close();
-		    if(existe == 1) {
-		    	try {
-		    		sentencia.executeUpdate("UPDATE tb_autores SET salario = " + salario + " WHERE id = " + id);
-		    		System.out.println("Salario actualizado.");
-		    		ResultSet result = sentencia.executeQuery("SELECT * FROM tb_autores WHERE id = " + id);
-		    		result.next();
-		    		System.out.println(result.getInt(1)+" "+result.getString(2)+" "+result.getString(3)+" "+result.getString(4)+" "+result.getString(5)+" "+result.getInt(6)+" "+result.getString(7)+" "+result.getString(8)+" "+result.getInt(9));
-		    		result.close();
-		    	} catch(SQLException sql) { System.out.println("No se ha podido actualizar el salario.\n" + sql.getMessage()); }
+		    int update = sentencia.executeUpdate("UPDATE tb_autores SET salario = " + salario + " WHERE id = " + id);
+		    if(update != 0) {
+			    System.out.println("Salario actualizado.");
+			    ResultSet result = sentencia.executeQuery("SELECT * FROM tb_autores WHERE id = " + id);
+			    result.next();
+			    System.out.println(result.getInt(1)+" "+result.getString(2)+" "+result.getString(3)+" "+result.getString(4)+" "+result.getString(5)+" "+result.getInt(6)+" "+result.getString(7)+" "+result.getString(8)+" "+result.getInt(9));
+			    result.close();
 		    } else {
 		    	System.out.println("El autor no existe.");
 		    }
+		    sc.close();
 		    sentencia.close();
 		    conexion.close();
 		} catch(SQLException e) { e.printStackTrace(); }
