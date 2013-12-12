f5242c9d8b7f1e35c07378778433f4c6d3f19a43
Fernando Quilez Esquitino <ferquies@gmail.com>
2013-12-12 08:10:36 +0100

    añadido ejercicio examen DINT y ejercicio Picturebox sin terminar

---
+++ DINT/Picturebox/Picturebox/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Picturebox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool pinta = false;
+        string brocha;
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (brocha == "pincel")
+            {
+                if (pinta)
+                {
+                    Graphics g = pictureBox1.CreateGraphics();
+                    g.FillEllipse(new SolidBrush(Color.Azure), e.X, e.Y, 10, 10);
+                    g.Dispose();
+                }
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            pinta = true;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            pinta = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            brocha = "pincel";
+        }
+    }
+}
---
+++ DINT/Prueba4_Examen/Prueba4_Examen/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba4_Examen
+{
+    public class Vector
+    {
+        private int[] datos;
+
+        public Vector(int longitud)
+        {
+            this.datos = new int[longitud];
+        }
+
+        public int this[int num]
+        {
+            get
+            {
+                return datos[num];
+            }
+            set
+            {
+                datos[num] = value;
+            }
+        }
+
+        public void eliminarRepetidos()
+        {
+            int[] aux = new int[datos.Length];
+            int[] sin_repetir = new int[datos.Length];
+            int[] repetidos = new int[500000];
+            for(int i = 0; i < repetidos.Length; i++)
+            {
+                repetidos[i] = 0;
+            }
+
+            for (int i = 0; i < datos.Length; i++)
+            {
+                repetidos[datos[i]]++;
+                aux[i] = datos[i];
+                if (repetidos[datos[i]] < 2)
+                {
+                    sin_repetir[i] = datos[i];
+                }
+            }
+
+            this.datos = sin_repetir;
+        }
+
+        public void mostrar()
+        {
+            for(int i = 0; i < datos.Length; i++)
+            {
+                Console.Write(datos[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v = new Vector(4);
+            v[0] = 1;
+            v[1] = 23;
+            v[2] = 1;
+            v[3] = 48;
+
+            v.mostrar();
+            v.eliminarRepetidos();
+            v.mostrar();
+            Console.ReadLine();
+        }
+    }
+}
