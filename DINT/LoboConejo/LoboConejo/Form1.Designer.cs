6e8d10baa46dbf4d3d72d7748d6c39d7eee6fb2f
Fernando Quilez Esquitino <ferquies@gmail.com>
2014-01-27 14:12:54 +0100

    añadido ejercicio DINT/LoboConejo

---
+++ DINT/LoboConejo/LoboConejo/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace LoboConejo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelLobo = new System.Windows.Forms.Label();
+            this.labelConejo = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelLobo
+            // 
+            this.labelLobo.AutoSize = true;
+            this.labelLobo.Location = new System.Drawing.Point(67, 55);
+            this.labelLobo.Name = "labelLobo";
+            this.labelLobo.Size = new System.Drawing.Size(31, 13);
+            this.labelLobo.TabIndex = 0;
+            this.labelLobo.Text = "Lobo";
+            // 
+            // labelConejo
+            // 
+            this.labelConejo.AutoSize = true;
+            this.labelConejo.Location = new System.Drawing.Point(237, 197);
+            this.labelConejo.Name = "labelConejo";
+            this.labelConejo.Size = new System.Drawing.Size(40, 13);
+            this.labelConejo.TabIndex = 1;
+            this.labelConejo.Text = "Conejo";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 406);
+            this.Controls.Add(this.labelConejo);
+            this.Controls.Add(this.labelLobo);
+            this.Name = "Form1";
+            this.Text = "LoboConejo";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelLobo;
+        private System.Windows.Forms.Label labelConejo;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ DINT/LoboConejo/LoboConejo/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LoboConejo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool up = false;
+        private bool down = false;
+        private bool left = false;
+        private bool right = false;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int xlobo = labelLobo.Location.X;
+            int ylobo = labelLobo.Location.Y;
+            int wlobo = labelLobo.Width;
+            int hlobo = labelLobo.Height;
+            int xconejo = labelConejo.Location.X;
+            int yconejo = labelConejo.Location.Y;
+            int wconejo = labelConejo.Width;
+            int hconejo = labelConejo.Height;
+
+            if (labelLobo.Location.X < labelConejo.Location.X) { xlobo++; }
+            if (labelLobo.Location.X > labelConejo.Location.X) { xlobo--; }
+            if (labelLobo.Location.Y < labelConejo.Location.Y) { ylobo++; }
+            if (labelLobo.Location.Y > labelConejo.Location.Y) { ylobo--; }
+
+            if (up) { yconejo -= 2; }
+            if (down) { yconejo += 2; }
+            if (left) { xconejo -= 2; }
+            if (right) { xconejo += 2; }
+
+            if (xlobo <= xconejo + wconejo && xlobo + wlobo > xconejo && ylobo < yconejo + hconejo && ylobo + hlobo > yconejo) { timer1.Stop(); MessageBox.Show("Perdiste"); }
+
+            labelLobo.Location = new Point(xlobo, ylobo);
+            labelConejo.Location = new Point(xconejo, yconejo);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up) { up = true; }
+            if (e.KeyCode == Keys.Down) { down = true; }
+            if (e.KeyCode == Keys.Left) { left = true; }
+            if (e.KeyCode == Keys.Right) { right = true; }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up) { up = false; }
+            if (e.KeyCode == Keys.Down) { down = false; }
+            if (e.KeyCode == Keys.Left) { left = false; }
+            if (e.KeyCode == Keys.Right) { right = false; }
+        }
+    }
+}
