5479d011a376458820b9b13da6bf9ffcf72b5dc5
Fernando Quilez Esquitino <ferquies@gmail.com>
2014-04-20 14:47:19 +0000

    pequeñas mejoras en PSER/automata.c

--- PSER/automata.c
+++ PSER/automata.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 
-#define N 33
+#define C 63
+#define F 22
 
 void main() {
-	int arr[N];
+	int arr[C];
 	int i, j, k, aux, first;
 	char cad[2], caux;
 
 	srand(getpid());
-	for(i = 0; i < N; i++) { arr[i] = rand() % 2; }
+
+	for(i = 0; i < C; i++) { arr[i] = rand() % 2; }
 		
-	for(k = 0; k < N*10; k++) {
+	for(k = 0; k < C*10; k++) {
 		system("clear");
 		printf("\t\t\tAutomata celular\n");
-		for(j = 0; j < N; j++) { 
+		for(j = 0; j < F; j++) { 
 			first = arr[0];
-			for(i = 0; i < N; i++) {
-				if(i == N-1) {
-					cad[0] = (char)(((int)'0')+arr[N-1]);
+			for(i = 0; i < C; i++) {
+				if(i == C-1) {
+					cad[0] = (char)(((int)'0')+arr[C-1]);
 					cad[1] = (char)(((int)'0')+first);
 				} else {
 					cad[0] = (char)(((int)'0')+arr[i]);
@@ -25,16 +27,16 @@ void main() {
 				}
 		
 				if((cad[0] == '0') && (cad[1] == '0')) { aux = 0; caux = '@'; }
-				if((cad[0] == '0') && (cad[1] == '1')) { aux = 1; caux = '#'; }
+				if((cad[0] == '0') && (cad[1] == '1')) { aux = 1; caux = ' '; }
 				if((cad[0] == '1') && (cad[1] == '0')) { aux = 1; caux = '|'; }
-				if((cad[0] == '1') && (cad[1] == '1')) { aux = 0; caux = '^'; }
+				if((cad[0] == '1') && (cad[1] == '1')) { aux = 0; caux = '_'; }
 			
 				arr[i] = aux;
 		
-				printf("%c ", caux);
+				printf("%c", caux);
 			}
 			printf("\n");
 		}
-		usleep(110000);
+		usleep(120000);
 	}
 }
