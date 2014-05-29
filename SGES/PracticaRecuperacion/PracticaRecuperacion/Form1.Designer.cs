e88992b44d82ee0b4912657d88520cae0433d2de
Fernando Quilez Esquitino <ferquies@gmail.com>
2014-05-29 20:52:00 +0200

    añadido SGES/PracticaRecuperacion inicial, con clases Product, Ticket y Cash

---
+++ SGES/PracticaRecuperacion/PracticaRecuperacion/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace PracticaRecuperacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(734, 497);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Stock";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(742, 523);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.Tag = "";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(734, 497);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Mesas";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(734, 497);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Administración";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 523);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Sistema de gestión empresarial";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage3;
+
+    }
+}
+
---
+++ SGES/PracticaRecuperacion/PracticaRecuperacion/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PracticaRecuperacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            /*Cash cash;
+            DialogResult open = MessageBox.Show("¿Abrir caja ahora?", "TPV", MessageBoxButtons.YesNo);
+            if(open == DialogResult.Yes)
+            cash = new Cash(200);
+            if (!cash.Open())
+                MessageBox.Show("Error al abrir la caja", "TPV", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            */
+        }
+
+        /*Ticket ticket = new Ticket();
+        Product cocacola = new Product("Coca-Cola", "Refresco de cola con gas", "C:/Documents and Settings/Fer/Mis documentos/Visual Studio 2010/Projects/PracticaRecuperacion/PracticaRecuperacion/images/cocacola.gif", "C:/Documents and Settings/Fer/Mis documentos/Visual Studio 2010/Projects/PracticaRecuperacion/PracticaRecuperacion/images/cocacola.gif", 1.5, 21, 10);
+        Product cocacola2 = new Product("Coca-Cola", "Refresco de cola con gas", "C:/Documents and Settings/Fer/Mis documentos/Visual Studio 2010/Projects/PracticaRecuperacion/PracticaRecuperacion/images/cocacola.gif", "C:/Documents and Settings/Fer/Mis documentos/Visual Studio 2010/Projects/PracticaRecuperacion/PracticaRecuperacion/images/cocacola.gif", 1.7, 21, 10);
+        */
+    }
+    class Product
+    {
+        private string name;
+        private string description;
+        private Image photo;
+        private Image tpv_image;
+        private double prize;
+        private int iva;
+        private int units;
+
+        public Product(string name, string description, string photo, string tpv_image, double prize, int iva, int units)
+        {
+            this.name = name;
+            this.description = description;
+            this.photo = Image.FromFile(photo);
+            this.tpv_image = Image.FromFile(tpv_image);
+            this.prize = prize;
+            this.iva = iva;
+            this.units = units;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Desciption
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        public Image Photo
+        {
+            get { return photo; }
+            set { photo = value; }
+        }
+        public Image TPV_Image
+        {
+            get { return tpv_image; }
+            set { tpv_image = value; }
+        }
+        public double Prize
+        {
+            get { return prize; }
+            set { prize = value; }
+        }
+        public int IVA
+        {
+            get { return iva; }
+            set { iva = value; }
+        }
+        public int Units
+        {
+            get { return units; }
+            set { units = value; }
+        }
+    }
+    class Ticket
+    {
+        private List<Product> products;
+        private bool opened = true;
+
+        public Ticket() { products = new List<Product>(); }
+        
+        public bool Add(Product product)
+        {
+            bool result = false;
+
+            if (opened)
+            {
+                if (product.Units > 0)
+                {
+                    products.Add(product);
+                    product.Units--;
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+        public void Remove(Product product)
+        {
+            if (opened)
+            {
+                products.Remove(product);
+                product.Units++;
+            }
+        }
+        public int Count() { return products.Count; }
+        public double Bill()
+        {
+            double bill = 0;
+            for (int i = 0; i < products.Count(); i++)
+            {
+                bill += products[i].Prize;
+            }
+            return bill;
+        }
+        public double BillTax()
+        {
+            double bill = 0;
+            for (int i = 0; i < products.Count(); i++)
+            {
+                bill += products[i].Prize;
+                bill += products[i].Prize / 100 * products[i].IVA;
+            }
+            return bill;
+        }
+    }
+    class Cash
+    {
+        private bool status;
+        private double amount;
+        private double default_amount;
+        private List<Ticket> ticket_list = new List<Ticket>();
+
+        public Cash(int default_amount)
+        {
+            this.status = false;
+            this.default_amount = default_amount;
+        }
+
+        public bool Open()
+        {
+            bool result = false;
+            if (!this.status)
+            {
+                this.status = true;
+                this.amount = this.default_amount;
+                result = true;
+            }
+            return result;
+        }
+        public bool Close()
+        {
+            bool result = false;
+            if (this.status)
+            {
+                this.amount = this.default_amount;
+                this.ticket_list = null;
+                result = true;
+            }
+            return result;
+        }
+        public bool Charge(Ticket ticket)
+        {
+            bool result = false;
+            bool allow = true;
+            for (int t = 0; t < this.ticket_list.Count; t++)
+            {
+                if (this.ticket_list[t] == ticket)
+                    allow = false;
+            }
+            if(allow)
+                this.amount += ticket.BillTax();
+            return result;
+        }
+    }
+}
